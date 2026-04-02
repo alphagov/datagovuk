b96e116b14fecc3fbe47303dd582bd798015765f
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-04-02 16:18:42 +0100

    DGUK-342 Add home page e2e tests

---
+++ tests/e2e/test_home_page.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import expect
+
+
+class TestHomePage:
+    COLLECTIONS = {
+        "Business and economy": "/collections/business-and-economy",
+        "Environment": "/collections/environment",
+        "Government": "/collections/government",
+        "Land and property": "/collections/land-and-property",
+        "People": "/collections/people",
+        "Transport": "/collections/transport",
+    }
+
+    def test_homepage_heading(self, page, live_server_url):
+        page.goto(live_server_url)
+        main_content = page.locator(".datagovuk-main")
+        expect(main_content.get_by_role("heading", level=1)).to_have_text(
+            "The home of UK public data to inform decisions and build services",
+        )
+
+    def test_homepage_collections_links(self, page, live_server_url):
+        page.goto(live_server_url)
+        collection_items = page.locator(".datagovuk-home-collections__items")
+        for name, href in self.COLLECTIONS.items():
+            link = collection_items.get_by_role("link", name=name, exact=True)
+            expect(link).to_have_attribute("href", href)
+
+    def test_homepage_footer_links(self, page, live_server_url):
+        page.goto(live_server_url)
+        expect(page.get_by_role("link", name="Data manual")).to_be_visible()
+        expect(page.get_by_role("link", name="Directory")).to_be_visible()
--- tests/e2e/test_sample_pages.py
+++ tests/e2e/test_sample_pages.py
@@ -1,13 +1,8 @@
+from django.urls import reverse
 from playwright.sync_api import expect
 
 
 class TestPages:
-    def test_homepage(self, page, live_server_url):
-        page.goto(live_server_url)
-
-        expect(page.get_by_role("heading", level=1)).to_have_text("data.gov.uk: Home")
-
     def test_about_page(self, page, live_server_url):
-        page.goto(live_server_url + "/about/")
-
+        page.goto(live_server_url + reverse("pages:about"))
         expect(page.get_by_role("heading", level=1)).to_have_text("About data.gov.uk")
