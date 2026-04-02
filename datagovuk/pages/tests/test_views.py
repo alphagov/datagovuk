b4f3c409fe85a9280c53f458e2fab9e4c04a5d60
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-04-02 11:30:51 +0100

    Add homepage title
    Add unique page title for the home page
    
    Add test

--- datagovuk/pages/tests/test_views.py
+++ datagovuk/pages/tests/test_views.py
@@ -9,3 +9,11 @@ def test_components(client):
 
     assert response.status_code == HTTPStatus.OK
     assert "Components - data.gov.uk" in response.content.decode()
+
+
+def test_home(client):
+    url = reverse("pages:home")
+    response = client.get(url)
+
+    assert response.status_code == HTTPStatus.OK
+    assert "data.gov.uk - The home of UK public data" in response.content.decode()
