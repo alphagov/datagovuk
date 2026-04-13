7c988cac92dca3085720e64402576dd5050fae60
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-04-13 16:00:00 +0100

    Add mechanism to render markdown to HTML for RenderedMarkdownView

--- datagovuk/core/markdown.py
+++ datagovuk/core/markdown.py
@@ -1,6 +1,61 @@
 from pathlib import Path
 
 import frontmatter
+import mistune
+
+
+class MarkdownToHTMLRenderer(mistune.HTMLRenderer):
+    def link(self, text, url, title=None):
+        return f'<a href="{url}" class="govuk-link datagovuk-link datagovuk-link--secondary">{text}</a>'
+
+    def table(self, text):
+        return f'<table class="govuk-table">\n{text}</table>'
+
+    def table_head(self, text):
+        return f'<thead class="govuk-table__head">{text}</thead>'
+
+    def table_body(self, text):
+        return f'<tbody class="govuk-table__body">{text}</tbody>'
+
+    def table_row(self, text):
+        return f'<tr class="govuk-table__row">{text}</tr>'
+
+    def table_cell(self, text, head=False, align=None):  # noqa: FBT002
+        if head:
+            return f'<th class="govuk-table__header">{text}</th>'
+        return f'<td class="govuk-table__cell">{text}</td>'
+
+    def heading(self, text, level):
+        valid_header_sizes = ["xl", "l", "m", "s"]
+
+        start_size = self.headings_start_with if self.headings_start_with in valid_header_sizes else "xl"
+        start_size_index = valid_header_sizes.index(start_size)
+
+        # Calculate index: starting index + (current h level - 1)
+        target_index = start_size_index + (level - 1)
+
+        header_size = "s"
+        if target_index < len(valid_header_sizes):
+            header_size = valid_header_sizes[target_index]
+
+        return f'<h{level} class="govuk-heading-{header_size} datagovuk-heading-{header_size}">{text}</h{level}>'
+
+    def paragraph(self, text):
+        return f'<p class="govuk-body-m datagovuk-body">{text}</p>'
+
+    def blockquote(self, text):
+        return f'<blockquote class="govuk-inset-text datagovuk-inset-text">{text}</blockquote>'
+
+    def list(self, text, ordered, depth=None):
+        if ordered:
+            return f'<ol class="govuk-list govuk-list--number datagovuk-list datagovuk-body">{text}</ol>'
+        return f'<ul class="govuk-list govuk-list--bullet datagovuk-list datagovuk-body">{text}</ul>'
+
+    def thematic_break(self):
+        return '<hr class="datagovuk-collection-header__underline">'
+
+
+render_markdown = mistune.create_markdown(renderer=MarkdownToHTMLRenderer())
 
 
 def _transform_context(value):
@@ -15,6 +70,5 @@ def get_template_context_from_markdown(markdown_file_path):
     with Path.open(markdown_file_path) as markdown_file:
         parsed_frontmatter = frontmatter.load(markdown_file)
         frontmatter_context = dict(parsed_frontmatter)
-        # TODO: Render markdown to HTML
-        frontmatter_context["content"] = parsed_frontmatter.content
+        frontmatter_context["content"] = render_markdown(parsed_frontmatter.content)
         return _transform_context(frontmatter_context)
