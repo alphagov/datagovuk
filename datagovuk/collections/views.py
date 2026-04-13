75fa820391b7914a2462f59ab2cd7884c95a8b17
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-04-13 16:00:00 +0100

    Add collection next/previous cards

--- datagovuk/collections/views.py
+++ datagovuk/collections/views.py
@@ -18,14 +18,20 @@ class CollectionPage(RenderedMarkdownView):
 
     @property
     def collection_pages(self):
-        return [
-            {
-                **collection_page,
-                "url": f"/collections/{self.kwargs['collection_name']}/{collection_page['slug']}",
-                "selected": collection_page["slug"] == self.kwargs["collection_page_name"],
-            }
-            for collection_page in COLLECTIONS[self.kwargs["collection_name"]]
-        ]
+        collection_pages = []
+        selected_index = 0
+        for index, collection_page in enumerate(COLLECTIONS[self.kwargs["collection_name"]]):
+            is_selected = collection_page["slug"] == self.kwargs["collection_page_name"]
+            collection_pages.append(
+                {
+                    **collection_page,
+                    "url": f"/collections/{self.kwargs['collection_name']}/{collection_page['slug']}",
+                    "selected": is_selected,
+                },
+            )
+            if is_selected:
+                selected_index = index
+        return collection_pages, selected_index
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -33,7 +39,12 @@ class CollectionPage(RenderedMarkdownView):
         context["collection"] = self.kwargs["collection_name"].replace("-", " ").capitalize()
         context["collection_slug"] = self.kwargs["collection_name"]
         context["page_last_updated"] = date.strptime(context["page_last_updated"], "%Y-%m-%d")
-        context["collection_pages"] = self.collection_pages
+        collection_pages, selected_index = self.collection_pages
+        if selected_index > 0:
+            context["previous_page"] = collection_pages[selected_index - 1]
+        if selected_index < len(collection_pages) - 1:
+            context["next_page"] = collection_pages[selected_index + 1]
+        context["collection_pages"] = collection_pages
         return context
 
 
