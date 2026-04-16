c9b95d41e083a65a52afcd203f2dcf8114b7cbb0
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-04-16 09:44:16 +0100

    DGUK-344 Add data manual content page
    Create data-manual pages (/data-manual/<data_manual_name>)
    
    Render the data-manual content markdown files
    
    Pass the rendered content into the data-manual pages
    
    Add the side navigation content for the data manual pages
    
    Add an attribute selected to define whether or not to highlight a link in the side nav
    
    Add unit and e2e tests

--- datagovuk/core/context_processors.py
+++ datagovuk/core/context_processors.py
@@ -1,3 +1,6 @@
+from datagovuk.data_manual.constants import DATA_MANUAL_PAGES
+
+
 def collections(request):
     return {
         "collections": [
@@ -33,64 +36,6 @@ def collections(request):
             },
         ],
     }
-
-
-DATA_MANUAL_PAGES = [
-    {
-        "title": "Who this manual is for",
-        "url": "/data-manual/who-this-manual-is-for",
-        "description": "Find out if this manual can help you and why it's important",
-        "icon": "user",
-    },
-    {
-        "title": "Data management",
-        "url": "/data-manual/data-management",
-        "description": "Understand data roles and responsibilities, and how to manage data quality",
-        "icon": "database",
-    },
-    {
-        "title": "Data standards",
-        "url": "/data-manual/data-standards",
-        "description": "A range of standards for improving how data is used across government",
-        "icon": "document",
-    },
-    {
-        "title": "Security",
-        "url": "/data-manual/security",
-        "description": "Strategies, policies and guidance to make your service safer",
-        "icon": "padlock",
-    },
-    {
-        "title": "Data protection and privacy",
-        "url": "/data-manual/data-protection-and-privacy",
-        "description": "How to comply with the Data Protection Act 2018 and UK GDPR",
-        "icon": "shield",
-    },
-    {
-        "title": "Data sharing",
-        "url": "/data-manual/data-sharing",
-        "description": "Frameworks and guides about sharing data between government organisations",
-        "icon": "sharing",
-    },
-    {
-        "title": "AI and data-driven technologies",
-        "url": "/data-manual/ai-and-data-driven-technologies",
-        "description": "How to use AI safely and effectively in government",
-        "icon": "stars",
-    },
-    {
-        "title": "APIs and technical guidance",
-        "url": "/data-manual/apis-and-technical-guidance",
-        "description": "How to build APIs in government and improve API standards",
-        "icon": "api",
-    },
-    {
-        "title": "General guidance",
-        "url": "/data-manual/general-guidance",
-        "description": "Where to start when you're creating a new public service",
-        "icon": "open-book",
-    },
-]
 
 
 def data_manual(request):
---
+++ datagovuk/data_manual/views.py
@@ -0,0 +1,31 @@
+from copy import deepcopy
+
+from datagovuk.core.views import RenderedMarkdownView
+
+from .constants import DATA_MANUAL_PAGES
+from .constants import DATA_MANUAL_PAGES_BY_SLUG
+
+
+class DataManualView(RenderedMarkdownView):
+    template_name = "data_manual/content.jinja"
+
+    def get_markdown_file_path(self):
+        return f"datagovuk/content/data-manual/{self.kwargs['data_manual_name']}.md"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = DATA_MANUAL_PAGES_BY_SLUG[self.kwargs["data_manual_name"]]["title"]
+        context["side_nav_items"] = self.side_nav_items
+
+        return context
+
+    @property
+    def side_nav_items(self):
+        data_manual_items = []
+        for item in deepcopy(DATA_MANUAL_PAGES):
+            if item["slug"] == self.kwargs["data_manual_name"]:
+                item["selected"] = True
+
+            data_manual_items.append(item)
+
+        return data_manual_items
