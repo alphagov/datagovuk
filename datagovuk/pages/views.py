659d0b42966aabd0e19252ed5d27c2e1057b66ed
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-04-20 11:02:48 +0100

    DGUK-345 Add title to the front matter for content-pages
    Add title as a frontmatter in the content-pages markdown files
    
    Remove the explicit adding of title to the context in the pages view as it's passed in
    via the RenderedMarkdownView

--- datagovuk/pages/views.py
+++ datagovuk/pages/views.py
@@ -6,8 +6,3 @@ class PagesView(RenderedMarkdownView):
 
     def get_markdown_file_path(self):
         return f"datagovuk/content/content-pages/{self.kwargs['slug']}.md"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = self.kwargs["title"]
-        return context
