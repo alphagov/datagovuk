305d01cdef45d2769123dc32aa62bf7895b743e6
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-04-27 16:22:06 +0100

    Ensure collections/data manual header works on 500 page

--- datagovuk/pages/views.py
+++ datagovuk/pages/views.py
@@ -1,5 +1,3 @@
-from django.core.exceptions import PermissionDenied, SuspiciousOperation
-
 from datagovuk.core.views import RenderedMarkdownView
 
 
@@ -8,27 +6,3 @@ class PagesView(RenderedMarkdownView):
 
     def get_markdown_file_path(self):
         return f"datagovuk/content/content-pages/{self.kwargs['slug']}.md"
-
-
-class Error400View(RenderedMarkdownView):
-    template_name = "pages/components.jinja"
-
-    def get_context_data(self, *args, **kwargs):
-        error_message = "Some bad request"
-        raise SuspiciousOperation(error_message)
-
-
-class Error403View(RenderedMarkdownView):
-    template_name = "pages/components.jinja"
-
-    def get_context_data(self, *args, **kwargs):
-        error_message = "Forbidden"
-        raise PermissionDenied(error_message)
-
-
-class Error500View(RenderedMarkdownView):
-    template_name = "pages/components.jinja"
-
-    def get_context_data(self, *args, **kwargs):
-        error_message = "Some exception"
-        raise KeyError(error_message)
