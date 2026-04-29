b06134782992314b5fc27a66c1149f4cd876c8be
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-04-29 13:37:16 +0100

    DGUK-351 Update test
    Update the google tag manager test to use the settings package to update
    the env variables

--- tests/e2e/test_google_tag_manager.py
+++ tests/e2e/test_google_tag_manager.py
@@ -2,7 +2,11 @@ import re
 
 
 class TestGoogleTagManager:
-    def test_google_tag_manager_is_present(self, page, live_server_url):
+    def test_google_tag_manager_is_present(self, page, live_server_url, settings):
+        settings.GOOGLE_TAG_MANAGER_ID = "UA-XXXXX-Y"
+        settings.GOOGLE_TAG_MANAGER_AUTH = "fake-auth-token"
+        settings.GOOGLE_TAG_MANAGER_PREVIEW = "fake-preview-token"
+
         regex = re.compile(
             r"https://www\.googletagmanager\.com/gtm\.js\?id=UA-XXXXX-Y&gtm_auth=fake-auth-token&gtm_preview=fake-preview-token&gtm_cookies_win=x",
         )
