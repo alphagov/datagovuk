2345ab68ae3d5f0d3907cc19232760989d5dacd0
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-04-30 11:35:00 +0100

    Stop setting cache control header for non-200 responses

--- datagovuk/core/tests/test_middleware.py
+++ datagovuk/core/tests/test_middleware.py
@@ -1,10 +1,13 @@
+from http import HTTPStatus
+
+import pytest
 from django.http import HttpResponse
 
 from datagovuk.core.middleware import CacheControlMiddleware
 
 
 class TestCacheControlMiddleware:
-    def test_cache_control_added_when_missing(self, rf):
+    def test_middleware_no_cache_control_cache_control_added(self, rf):
         def response(_request):
             return HttpResponse("ok")
 
@@ -15,7 +18,7 @@ class TestCacheControlMiddleware:
 
         assert cached_response.headers["Cache-Control"] == "max-age=1800, public"
 
-    def test_existing_cache_control_is_not_overridden(self, rf):
+    def test_middleware_cache_control_present_cache_control_not_added(self, rf):
         def response(_request):
             response = HttpResponse("ok")
             response.headers["Cache-Control"] = "no-store"
@@ -28,7 +31,7 @@ class TestCacheControlMiddleware:
 
         assert cached_response.headers["Cache-Control"] == "no-store"
 
-    def test_cache_control_uses_setting(self, rf, settings):
+    def test_middleware_cache_control_uses_setting(self, rf, settings):
         settings.CACHE_CONTROL_DEFAULT = "max-age=3600, public"
 
         def response(_request):
@@ -40,3 +43,27 @@ class TestCacheControlMiddleware:
         cached_response = cache_middleware(request)
 
         assert cached_response.headers["Cache-Control"] == "max-age=3600, public"
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [
+            HTTPStatus.INTERNAL_SERVER_ERROR,
+            HTTPStatus.BAD_REQUEST,
+            HTTPStatus.FORBIDDEN,
+            HTTPStatus.NOT_FOUND,
+        ],
+    )
+    def test_middleware_bad_status_code_no_cache_control_applied(self, status_code, rf, settings):
+        settings.CACHE_CONTROL_DEFAULT = "max-age=3600, public"
+
+        def response(_request):
+            response = HttpResponse("error")
+            response.status_code = status_code
+            return response
+
+        request = rf.get("/")
+
+        cache_middleware = CacheControlMiddleware(response)
+        cached_response = cache_middleware(request)
+
+        assert cached_response.headers.get("Cache-Control") is None
