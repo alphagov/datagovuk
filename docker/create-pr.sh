1d0a5b058a2364fe6f02c9a830d2b2ea32d02fd2
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-05-01 11:36:50 +0100

    DGUK-506 Add workflow to create dgu-charts PR
    Add teh create-pr workflow to create a dgu-charts/integration

---
+++ docker/create-pr.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -eux
+
+if [[ ${IS_TAG:-} = "true" ]]; then
+  export IMAGE_TAG="${GH_REF}"
+  export SOURCE_BRANCH="main"
+else
+  export IMAGE_TAG=$(gh api repos/alphagov/datagovuk/branches/${GH_REF} | jq .commit.sha -r)
+  export SOURCE_BRANCH=${GH_REF}
+fi
+
+git config --global user.email "govuk-ci@users.noreply.github.com"
+git config --global user.name "govuk-ci"
+
+git clone https://${GH_TOKEN}@github.com/alphagov/govuk-dgu-charts.git charts
+
+cd charts/charts/datagovuk/images
+
+for ENV in $(echo $ENVS | tr "," " "); do
+  (
+    cd "${ENV}"
+    yq -i '.tag = env(IMAGE_TAG)' "datagovuk.yaml"
+    yq -i '.branch = env(SOURCE_BRANCH)' "datagovuk.yaml"
+    git add "datagovuk.yaml"
+
+    if [[ $(git status | grep "nothing to commit") ]]; then
+      echo "Nothing to commit"
+    elif [[ "${DIRECT_PUSH:-false}" == "true" ]]; then
+      BRANCH="ci/${IMAGE_TAG}-${ENV}"
+      git checkout -b ${BRANCH}
+      git commit -m "Update datagovuk image tags for ${ENV} to ${IMAGE_TAG}"
+      git push --set-upstream origin "${BRANCH}"
+      PR_URL=$(gh pr create --title "Update datagovuk image tags for ${ENV} (${IMAGE_TAG})" --base main --head "${BRANCH}" --fill --repo alphagov/govuk-dgu-charts)
+      gh pr merge "${PR_URL}" --auto --merge --delete-branch
+    else
+      BRANCH="ci/${IMAGE_TAG}-${ENV}"
+
+      if git show-ref --quiet refs/heads/${BRANCH}; then
+        echo "Branch ${BRANCH} already exists on govuk-dgu-charts"
+      else
+        git checkout -b ${BRANCH}
+        git commit -m "Update datagovuk image tags for ${ENV} to ${IMAGE_TAG}"
+        git push --set-upstream origin "${BRANCH}"
+        gh pr create --title "Update datagovuk image tags for ${ENV} (${IMAGE_TAG})" --base main --head "${BRANCH}" --fill
+      fi
+    fi
+  )
+done
