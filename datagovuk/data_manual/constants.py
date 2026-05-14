be0b7ff3a63aa0a87a2f26f69c0b03e182c430c8
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-05-14 14:29:54 +0100

    Make data manual URLs more DRY

--- datagovuk/data_manual/constants.py
+++ datagovuk/data_manual/constants.py
@@ -1,69 +1,71 @@
 from django.urls import reverse_lazy
 
+
+def data_manual_url(data_manual_name):
+    return reverse_lazy("data_manual:data_manual_page", kwargs={"data_manual_name": data_manual_name})
+
+
 DATA_MANUAL_PAGES = [
     {
         "title": "Who this manual is for",
         "slug": "who-this-manual-is-for",
-        "url": reverse_lazy("data_manual:data_manual_page", kwargs={"data_manual_name": "who-this-manual-is-for"}),
+        "url": data_manual_url("who-this-manual-is-for"),
         "description": "Find out if this manual can help you and why it's important",
         "icon": "user",
     },
     {
         "title": "Data management",
         "slug": "data-management",
-        "url": reverse_lazy("data_manual:data_manual_page", kwargs={"data_manual_name": "data-management"}),
+        "url": data_manual_url("data-management"),
         "description": "Understand data roles and responsibilities, and how to manage data quality",
         "icon": "database",
     },
     {
         "title": "Data standards",
         "slug": "data-standards",
-        "url": reverse_lazy("data_manual:data_manual_page", kwargs={"data_manual_name": "data-standards"}),
+        "url": data_manual_url("data-standards"),
         "description": "A range of standards for improving how data is used across government",
         "icon": "document",
     },
     {
         "title": "Security",
         "slug": "security",
-        "url": reverse_lazy("data_manual:data_manual_page", kwargs={"data_manual_name": "security"}),
+        "url": data_manual_url("security"),
         "description": "Strategies, policies and guidance to make your service safer",
         "icon": "padlock",
     },
     {
         "title": "Data protection and privacy",
         "slug": "data-protection-and-privacy",
-        "url": reverse_lazy("data_manual:data_manual_page", kwargs={"data_manual_name": "data-protection-and-privacy"}),
+        "url": data_manual_url("data-protection-and-privacy"),
         "description": "How to comply with the Data Protection Act 2018 and UK GDPR",
         "icon": "shield",
     },
     {
         "title": "Data sharing",
         "slug": "data-sharing",
-        "url": reverse_lazy("data_manual:data_manual_page", kwargs={"data_manual_name": "data-sharing"}),
+        "url": data_manual_url("data-sharing"),
         "description": "Frameworks and guides about sharing data between government organisations",
         "icon": "sharing",
     },
     {
         "title": "AI and data-driven technologies",
         "slug": "ai-and-data-driven-technologies",
-        "url": reverse_lazy(
-            "data_manual:data_manual_page",
-            kwargs={"data_manual_name": "ai-and-data-driven-technologies"},
-        ),
+        "url": data_manual_url("ai-and-data-driven-technologies"),
         "description": "How to use AI safely and effectively in government",
         "icon": "stars",
     },
     {
         "title": "APIs and technical guidance",
         "slug": "apis-and-technical-guidance",
-        "url": reverse_lazy("data_manual:data_manual_page", kwargs={"data_manual_name": "apis-and-technical-guidance"}),
+        "url": data_manual_url("apis-and-technical-guidance"),
         "description": "How to build APIs in government and improve API standards",
         "icon": "api",
     },
     {
         "title": "General guidance",
         "slug": "general-guidance",
-        "url": reverse_lazy("data_manual:data_manual_page", kwargs={"data_manual_name": "general-guidance"}),
+        "url": data_manual_url("general-guidance"),
         "description": "Where to start when you're creating a new public service",
         "icon": "open-book",
     },
