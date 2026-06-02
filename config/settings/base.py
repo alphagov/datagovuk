1f849c422e5c2971cfd2148cf3ce8e4521ac77fe
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-06-02 17:53:18 +0100

    Add early years feature flag

--- config/settings/base.py
+++ config/settings/base.py
@@ -305,6 +305,7 @@ DATAGOVUK_GIT_SHA = env("GIT_SHA", default=None)
 
 class FEATURE_FLAGS(Enum):  # noqa: N801
     TEST_FEATURE_FLAG = "test-feature-flag"
+    EARLY_YEARS = "early-years"
 
 
 FEATURE_FLAGS_ENABLED = env.list("FEATURE_FLAGS_ENABLED", default=[])
