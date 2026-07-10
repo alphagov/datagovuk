c766c3c668d5011547eee094c5f07d612a968759
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-07-10 17:16:39 +0100

    Fix coverage complaint

--- datagovuk/directory/tests/test_solr.py
+++ datagovuk/directory/tests/test_solr.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 from django.core.exceptions import ImproperlyConfigured
 
@@ -8,3 +10,10 @@ def test_get_solr_client_no_solr_url(settings):
     settings.SOLR_URL = None
     with pytest.raises(ImproperlyConfigured):
         get_solr_client()
+
+
+@patch("datagovuk.directory.solr.pysolr")
+def test_get_solr_client_solr_url_set(pysolr_mock, settings):
+    settings.SOLR_URL = "http://solr.example.net"
+    client = get_solr_client()
+    assert client == pysolr_mock.Solr.return_value
