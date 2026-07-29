3670cde1c4637ceb283e8f74911050985969d385
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-07-29 10:30:10 +0100

    Drop datagovuk-specific macro wrappers for govuk macro customisation pattern

--- datagovuk/core/tests/test_jinja2.py
+++ datagovuk/core/tests/test_jinja2.py
@@ -1,28 +1,10 @@
-from datagovuk.core.jinja2 import combine
+from datagovuk.core.jinja2 import to_govuk_items
 
 
-def test_combine_flat_dicts():
-    dict_1 = {"foo": "bar"}
-    dict_2 = {"baz": "woop"}
-    result = combine(dict_1, dict_2)
-    assert result == {"foo": "bar", "baz": "woop"}
-
-
-def test_combine_flat_dict_and_deep_dict():
-    dict_1 = {"foo": "bar"}
-    dict_2 = {"baz": {"hello": "woop"}}
-    result = combine(dict_1, dict_2)
-    assert result == {"foo": "bar", "baz": {"hello": "woop"}}
-
-
-def test_combine_deep_dicts():
-    dict_1 = {"foo": {"1": "bar"}}
-    dict_2 = {"foo": {"2": "baz"}}
-    result = combine(dict_1, dict_2)
-    assert result == {"foo": {"1": "bar", "2": "baz"}}
-
-
-def test_combine_non_dict():
-    dict_1 = {"foo": {"1": "bar"}}
-    result = combine(dict_1, None)
-    assert result == dict_1
+def test_to_govuk_items_returns_items_list():
+    choices = [("value", "Label"), ("value-2", "Label 2")]
+    items = to_govuk_items(choices)
+    assert items == [
+        {"text": "Label", "value": "value"},
+        {"text": "Label 2", "value": "value-2"},
+    ]
