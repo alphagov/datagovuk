3679c645061030218b35cf8836fad14342896394
Obsiye <abdirizak.obsiye@dsit.gov.uk>
2026-07-31 14:47:21 +0100

    Test parse_isodate
    Test parse_isodate which is used in the additional_information.jinja to parse a string with a date in it

--- datagovuk/core/tests/test_jinja2.py
+++ datagovuk/core/tests/test_jinja2.py
@@ -1,4 +1,4 @@
-from datagovuk.core.jinja2 import format_file_size, to_govuk_items
+from datagovuk.core.jinja2 import format_file_size, parse_isodate, to_govuk_items
 
 
 def test_to_govuk_items_returns_items_list():
@@ -24,3 +24,19 @@ def test_format_file_size_returns_gb():
 
 def test_format_file_size_large_size_falls_through_to_gb():
     assert format_file_size(1024 * 1024 * 1024 * 1024) == "1024 GB"
+
+def test_parse_isodate_returns_datetime_for_valid_iso_string():
+    result = parse_isodate("2011-11-04T00:05:23.12345Z")
+    assert result == datetime(2011, 11, 4, 0, 5, 23, 123450, tzinfo=UTC)
+
+
+def test_parse_isodate_returns_none_for_empty_string():
+    assert parse_isodate("") is None
+
+
+def test_parse_isodate_returns_none_for_none():
+    assert parse_isodate(None) is None
+
+
+def test_parse_isodate_returns_none_for_invalid_value():
+    assert parse_isodate("not-a-date") is None
