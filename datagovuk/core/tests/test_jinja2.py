ec975399ff908918da730125036bb1c82d749c07
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-08-06 16:14:57 +0100

    Drop strip_markdown

--- datagovuk/core/tests/test_jinja2.py
+++ datagovuk/core/tests/test_jinja2.py
@@ -1,4 +1,4 @@
-from datagovuk.core.jinja2 import format_file_size, sanitize_html, strip_markdown, to_govuk_items
+from datagovuk.core.jinja2 import format_file_size, sanitize_html, to_govuk_items
 
 
 def test_to_govuk_items_returns_items_list():
@@ -57,61 +57,3 @@ def test_sanitize_html_removes_disallowed_attributes_on_a_tag():
 def test_sanitize_html_removes_unexpected_tags_with_attributes():
     result = sanitize_html('<span style="color:red" class="foo">text</span>')
     assert str(result) == "text"
-
-
-def test_strip_markdown_empty_string_returns_empty():
-    assert strip_markdown("") == ""
-
-
-def test_strip_markdown_none_returns_empty():
-    assert strip_markdown(None) == ""
-
-
-def test_strip_markdown_removes_images():
-    result = strip_markdown("See ![alt text](https://example.com/image.png) for more")
-    assert result == "See  for more"
-
-
-def test_strip_markdown_removes_links():
-    result = strip_markdown("Visit [Google](https://google.com) now")
-    assert result == "Visit Google now"
-
-
-def test_strip_markdown_removes_inline_code():
-    result = strip_markdown("Use the `print` function")
-    assert result == "Use the print function"
-
-
-def test_strip_markdown_removes_bullet_list_markers():
-    result = strip_markdown("* Item one\n* Item two\n* Item three")
-    assert result == "Item one\nItem two\nItem three"
-
-
-def test_strip_markdown_removes_header_markers():
-    result = strip_markdown("# Header One\n## Header Two\n### Header Three")
-    assert result == "Header One\nHeader Two\nHeader Three"
-
-
-def test_strip_markdown_removes_bold_double_asterisk():
-    result = strip_markdown("This is **bold** text")
-    assert result == "This is bold text"
-
-
-def test_strip_markdown_removes_bold_double_underscore():
-    result = strip_markdown("This is __bold__ text")
-    assert result == "This is bold text"
-
-
-def test_strip_markdown_removes_italics_single_asterisk():
-    result = strip_markdown("This is *italic* text")
-    assert result == "This is italic text"
-
-
-def test_strip_markdown_removes_italics_single_underscore():
-    result = strip_markdown("This is _italic_ text")
-    assert result == "This is italic text"
-
-
-def test_strip_markdown_strips_whitespace_from_result():
-    result = strip_markdown("  # Header  ")
-    assert result == "Header"
