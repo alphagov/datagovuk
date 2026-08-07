77d0c767eadf76acd9ebb221b4f6bd4644778849
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-08-07 17:22:06 +0100

    Add a line-clamp-toggle javascript component for dataset summary

---
+++ datagovuk/static/javascripts/line-clamp-toggle.js
@@ -0,0 +1,30 @@
+class DatagovukLineClampToggle {
+  constructor($button) {
+    this.$button = $button
+    this.$showTarget = document.querySelector("#" + this.$button.getAttribute("aria-controls"))
+    this.clampClass = "datagovuk-line-clamp"
+    this.$showTarget.classList.add(this.clampClass)
+    this.$button.setAttribute("aria-expanded", "false")
+    this.$button.addEventListener("click", () => this.toggle())
+  }
+
+  toggle() {
+    const isExpanded = this.$button.getAttribute("aria-expanded") === "true"
+
+    if (isExpanded) {
+      this.$showTarget.classList.add(this.clampClass)
+      this.$button.setAttribute("aria-expanded", "false")
+      this.$button.textContent = "Show more"
+    } else {
+      this.$showTarget.classList.remove(this.clampClass)
+      this.$button.setAttribute("aria-expanded", "true")
+      this.$button.textContent = "Show less"
+    }
+  }
+}
+
+// Initialize
+const $clampButtons = document.querySelectorAll('.datagovuk-line-clamp-toggle')
+$clampButtons.forEach(($button) => {
+  new DatagovukLineClampToggle($button)
+})
