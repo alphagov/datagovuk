a9c20f8f05118e20939ddfcb07b1bcddb16fe33e
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-08-07 14:11:33 +0100

    Drop trailing slashes from directory URLs to fully align them with rails app

--- datagovuk/directory/urls.py
+++ datagovuk/directory/urls.py
@@ -9,32 +9,32 @@ app_name = "directory"
 
 urlpatterns = [
     path(
-        "search/",
+        "search",
         flag_required(settings.FEATURE_FLAGS.SOLR_SEARCH, views.SearchView.as_view()),
         name="search",
     ),
     path(
-        "dataset/<uuid:uuid>/<slug:slug>/",
+        "dataset/<uuid:uuid>/<slug:slug>",
         flag_required(settings.FEATURE_FLAGS.SOLR_SEARCH, views.DatasetView.as_view()),
         name="dataset",
     ),
     path(
-        "dataset/<uuid:dataset_uuid>/<slug:name>/datafile/<uuid:datafile_uuid>/preview/",
+        "dataset/<uuid:dataset_uuid>/<slug:name>/datafile/<uuid:datafile_uuid>/preview",
         flag_required(settings.FEATURE_FLAGS.SOLR_SEARCH, views.PreviewView.as_view()),
         name="preview",
     ),
     path(
-        "dataset/<str:legacy_dataset_name>/",
+        "dataset/<str:legacy_dataset_name>",
         views.LegacyDatasetRedirectView.as_view(),
         name="legacy_dataset",
     ),
     path(
-        "dataset/<str:legacy_dataset_name>/resource/<str:datafile_uuid>/",
+        "dataset/<str:legacy_dataset_name>/resource/<str:datafile_uuid>",
         views.LegacyDatafileRedirectView.as_view(),
         name="legacy_datafile",
     ),
     path(
-        "data/search/",
+        "data/search",
         views.LegacySearchRedirectView.as_view(),
         name="legacy_search",
     ),
