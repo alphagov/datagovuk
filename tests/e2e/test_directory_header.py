0ed299c8e8bc4b6921ee296c25251ece98af668f
Obsiye <abdirizak.obsiye@dsit.gov.uk>
2026-08-12 10:53:51 +0100

    Add e2e test

---
+++ tests/e2e/test_directory_header.py
@@ -0,0 +1,23 @@
+import pytest
+from playwright.sync_api import expect
+
+
+@pytest.fixture(autouse=True)
+def enable_solr_feature_flag(settings):
+    settings.FEATURE_FLAGS_ENABLED = ["solr-search"]
+
+
+def test_directory_header_publish_your_data_links_to_publishers_page(page, live_server_url):
+    page.goto(live_server_url + "/search")
+    expect(page.locator(".datagovuk-directory-header__link", has_text="Publish your data")).to_have_attribute(
+        "href",
+        "/publishers/",
+    )
+
+
+def test_directory_header_publish_your_data_mobile_menu_links_to_publishers_page(mobile_page, live_server_url):
+    mobile_page.goto(live_server_url + "/search")
+    mobile_page.locator("#datagovuk-directory-header-button-all").click()
+    expect(
+        mobile_page.locator(".datagovuk-directory-menu .datagovuk-menu__item-link", has_text="Publish your data"),
+    ).to_have_attribute("href", "/publishers/")
