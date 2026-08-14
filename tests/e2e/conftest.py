6867b2f898ce7eda78445c98a33e93bc12cf1ee6
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-08-14 11:22:09 +0100

    Introduce new pytest.mark.devices approach for desktop/mobile E2E tests

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -2,7 +2,6 @@ import os
 
 import pytest
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from playwright.sync_api import sync_playwright
 
 from datagovuk.directory.e2e_fixtures import create_e2e_fixtures, delete_e2e_fixtures
 
@@ -43,8 +42,7 @@ def setup_suite():
 
 
 @pytest.fixture(scope="session")
-def browser():
-    playwright = sync_playwright().start()
+def browser(playwright):
 
     # If PLAYWRIGHT_HOST is set to something other than 127.0.0.1,
     # connect to a remote Playwright browser server (e.g. Docker container).
@@ -61,22 +59,52 @@ def browser():
     playwright.stop()
 
 
+def pytest_generate_tests(metafunc):
+    """
+    Automatically parameterize tests marked with @pytest.mark.devices(['desktop', 'mobile']).
+    """
+    if "device_name" in metafunc.fixturenames:
+        marker = metafunc.definition.get_closest_marker("devices")
+        devices = marker.args[0] if marker else ["desktop"]  # default to desktop only
+        metafunc.parametrize("device_name", devices, indirect=True)
+
+
 @pytest.fixture
-def page(browser, request):
-    context_kwargs = {}
+def device_name(request):
+    return getattr(request, "param", "desktop")
+
+
+@pytest.fixture
+def browser_context_args(device_name, playwright):
+    args = {}
+
     if E2E_BASIC_AUTH_USERNAME and E2E_BASIC_AUTH_PASSWORD:
-        context_kwargs["http_credentials"] = {
+        args["http_credentials"] = {
             "username": E2E_BASIC_AUTH_USERNAME,
             "password": E2E_BASIC_AUTH_PASSWORD,
         }
-    context = browser.new_context(**context_kwargs)
+
+    if device_name == "mobile":
+        args.update(playwright.devices["Pixel 5"])
+    else:
+        args["viewport"] = {"width": 1280, "height": 720}
+
+    return args
+
+
+@pytest.fixture
+def page(browser, browser_context_args, request):
+    context = browser.new_context(**browser_context_args)
     context.tracing.start(screenshots=True, snapshots=True)
+
     page = context.new_page()
     yield page
+
     if request.node.rep_call.failed:
         context.tracing.stop(path=f"e2e-failure-traces/trace-{request.node.name}.zip")
     else:
         context.tracing.stop()
+
     page.close()
     context.close()
 
--- tests/e2e/directory/test_search.py
+++ tests/e2e/directory/test_search.py
@@ -10,31 +10,25 @@ def search_url():
 
 
 class TestSearch:
-    @pytest.mark.parametrize(
-        "lazy_page",
-        [
-            lf("page"),
-            lf("mobile_page"),
-        ],
-    )
+    @pytest.mark.devices(["desktop", "mobile"])
     def test_search_query_only(
         self,
-        lazy_page,
+        page,
         live_server_url,
         search_url,
     ):
-        lazy_page.goto(live_server_url + search_url)
-        expect(lazy_page.get_by_role("heading", level=1, name="Directory")).to_be_visible()
-        lazy_page.get_by_role("textbox", name="Search directory").click()
-        lazy_page.get_by_role("textbox", name="Search directory").fill('"E2E Search"')
-        lazy_page.get_by_role("button", name="Search").click()
-        expect(lazy_page.get_by_text("1 to 6 of 6 results")).to_be_visible()
-        expect(lazy_page.get_by_role("link", name="DGUK E2E Search dataset licence")).to_be_visible()
-        expect(lazy_page.get_by_role("link", name="DGUK E2E Search dataset publisher")).to_be_visible()
-        expect(lazy_page.get_by_role("link", name="DGUK E2E Search dataset generic")).to_be_visible()
-        expect(lazy_page.get_by_role("link", name="DGUK E2E Search dataset format")).to_be_visible()
-        expect(lazy_page.get_by_role("link", name="DGUK E2E Search dataset topic")).to_be_visible()
-        expect(lazy_page.get_by_role("link", name="DGUK E2E Search dataset recent")).to_be_visible()
+        page.goto(live_server_url + search_url)
+        expect(page.get_by_role("heading", level=1, name="Directory")).to_be_visible()
+        page.get_by_role("textbox", name="Search directory").click()
+        page.get_by_role("textbox", name="Search directory").fill('"E2E Search"')
+        page.get_by_role("button", name="Search").click()
+        expect(page.get_by_text("1 to 6 of 6 results")).to_be_visible()
+        expect(page.get_by_role("link", name="DGUK E2E Search dataset licence")).to_be_visible()
+        expect(page.get_by_role("link", name="DGUK E2E Search dataset publisher")).to_be_visible()
+        expect(page.get_by_role("link", name="DGUK E2E Search dataset generic")).to_be_visible()
+        expect(page.get_by_role("link", name="DGUK E2E Search dataset format")).to_be_visible()
+        expect(page.get_by_role("link", name="DGUK E2E Search dataset topic")).to_be_visible()
+        expect(page.get_by_role("link", name="DGUK E2E Search dataset recent")).to_be_visible()
 
     @pytest.mark.parametrize(
         "lazy_page",
