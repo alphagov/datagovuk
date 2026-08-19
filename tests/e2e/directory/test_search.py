bbb80eb8cc8b126d0b494cfcaa15d272059be262
Joe Lanman <joelanman@gmail.com>
2026-08-19 18:28:34 +0100

    fix tests

--- tests/e2e/directory/test_search.py
+++ tests/e2e/directory/test_search.py
@@ -21,7 +21,7 @@ class TestSearch:
         page.get_by_role("textbox", name="Search directory").click()
         page.get_by_role("textbox", name="Search directory").fill('"E2E Search"')
         page.get_by_role("button", name="Search").click()
-        expect(page.get_by_text("1 to 6 of 6 results")).to_be_visible()
+        expect(page.get_by_text("6 results")).to_be_visible()
         expect(page.get_by_role("link", name="DGUK E2E Search dataset licence")).to_be_visible()
         expect(page.get_by_role("link", name="DGUK E2E Search dataset publisher")).to_be_visible()
         expect(page.get_by_role("link", name="DGUK E2E Search dataset generic")).to_be_visible()
@@ -41,7 +41,7 @@ class TestSearch:
         page.get_by_role("textbox", name="Search directory").click()
         page.get_by_role("textbox", name="Search directory").fill('"E2E Search"')
         page.get_by_role("button", name="Search").click()
-        expect(page.get_by_text("1 to 6 of 6 results")).to_be_visible()
+        expect(page.get_by_text("6 results")).to_be_visible()
         page.get_by_label("Sort by").select_option("recent")
         results = page.locator(".datagovuk-search-results__item")
         expect(results.nth(0).get_by_role("link", name="DGUK E2E Search dataset recent")).to_be_visible()
@@ -63,7 +63,7 @@ class TestSearch:
         page.get_by_role("combobox", name="Publisher").click()
         page.get_by_role("option", name="E2e publisher 2").click()
         page.get_by_role("button", name="Apply filters").click()
-        expect(page.get_by_text("1 to 1 of 1 results")).to_be_visible()
+        expect(page.get_by_text("1 result")).to_be_visible()
         expect(page.get_by_role("link", name="DGUK E2E Search dataset publisher")).to_be_visible()
 
     @pytest.mark.smoke
@@ -83,7 +83,7 @@ class TestSearch:
         page.get_by_role("combobox", name="Topic").click()
         page.get_by_role("option", name="Environment").click()
         page.get_by_role("button", name="Apply filters").click()
-        expect(page.get_by_text("1 to 1 of 1 results")).to_be_visible()
+        expect(page.get_by_text("1 result")).to_be_visible()
         expect(page.get_by_role("link", name="DGUK E2E Search dataset topic")).to_be_visible()
 
     @pytest.mark.smoke
@@ -103,7 +103,7 @@ class TestSearch:
         page.get_by_role("combobox", name="Format").click()
         page.get_by_role("option", name="CSV").click()
         page.get_by_role("button", name="Apply filters").click()
-        expect(page.get_by_text("1 to 1 of 1 results")).to_be_visible()
+        expect(page.get_by_text("1 result")).to_be_visible()
         expect(page.get_by_role("link", name="DGUK E2E Search dataset format")).to_be_visible()
 
     @pytest.mark.smoke
@@ -122,5 +122,5 @@ class TestSearch:
         page.wait_for_timeout(1000)
         page.get_by_role("checkbox", name="Open Government Licence (OGL").check()
         page.get_by_role("button", name="Apply filters").click()
-        expect(page.get_by_text("1 to 1 of 1 results")).to_be_visible()
+        expect(page.get_by_text("1 result")).to_be_visible()
         expect(page.get_by_role("link", name="DGUK E2E Search dataset licence")).to_be_visible()
